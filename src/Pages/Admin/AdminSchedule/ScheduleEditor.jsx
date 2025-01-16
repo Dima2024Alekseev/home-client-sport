a01cf71b58346d8447bda4f38d28fb2df430f0b1
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-01-16 23:32:13 +0700

    done

--- src/Pages/Admin/AdminSchedule/ScheduleEditor.jsx
+++ src/Pages/Admin/AdminSchedule/ScheduleEditor.jsx
@@ -12,7 +12,7 @@ const ScheduleEditor = () => {
     const fetchSchedule = async () => {
       try {
         const token = localStorage.getItem('token');
-        const response = await axios.get('http://localhost:5000/api/schedule', {
+        const response = await axios.get('/api/schedule', {
           headers: {
             'Authorization': token
           }
@@ -36,7 +36,7 @@ const ScheduleEditor = () => {
   const handleSave = async () => {
     try {
       const token = localStorage.getItem('token');
-      await axios.put('http://localhost:5000/api/schedule', { schedule: scheduleData }, {
+      await axios.put('/api/schedule', { schedule: scheduleData }, {
         headers: {
           'Authorization': token
         }
