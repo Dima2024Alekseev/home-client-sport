31e7e2afd7ee8547ee3887eba4caf0c0a47f571e
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-01-17 20:24:28 +0700

    done

--- src/Pages/Account-Authorization.jsx
+++ src/Pages/Account-Authorization.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useNotification } from '../Components/NotificationContext';
@@ -8,19 +8,21 @@ import Form from "../Components/Form";
 import Footer from "../Components/Footer/Footer";
 
 const Authorization = () => {
+  const [error, setError] = useState(null);
   const { showNotification } = useNotification();
   const navigate = useNavigate();
 
   const handleSubmit = async (formData) => {
     try {
-      const response = await axios.post('http://localhost:5000/api/admin/login', formData);
+      const response = await axios.post('/api/admin/login', formData, { withCredentials: true });
       const { token } = response.data;
       localStorage.setItem('token', token);
       localStorage.setItem('isAuthenticated', 'true');
       showNotification('Успешный вход!', 'success');
       navigate('/admin-dashboard');
     } catch (error) {
-      showNotification('Неверный логин или пароль', 'error');
+      setError(error.response?.data?.error || '');
+      showNotification('Ошибка авторизации', 'error');
     }
   };
 
@@ -34,6 +36,7 @@ const Authorization = () => {
           title_button='Войти'
           onSubmit={handleSubmit}
         />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </main>
       <Footer />
     </div>
