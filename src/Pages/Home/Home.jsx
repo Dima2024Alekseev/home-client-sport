f1d2686331ffbbd6ae6b8ee81edff0190b368ac8
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-02-02 23:43:53 +0700

    done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import NewsBlock from "../../Components/News-Block/News-Block";
 import StoreBlock from "../../Components/Store-Block/Store-Block";
 import Contact from "../../Components/Contact-Information/Contact-Information";
 import Footer from "../../Components/Footer/Footer";
-import postsData from "../../data/posts.json";
+import axios from "axios";
 import home_page_video from "../../video/club_3.mp4";
 
 const Home = () => {
@@ -15,7 +15,8 @@ const Home = () => {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                setPosts(postsData);
+                const response = await axios.get('/api/posts');
+                setPosts(response.data);
             } catch (error) {
                 console.error('Ошибка при получении данных:', error);
             }
