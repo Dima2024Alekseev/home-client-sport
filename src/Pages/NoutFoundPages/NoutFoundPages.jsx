a99302b23c9085c863a950a607d03852a8e5dc7e
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-02-17 18:34:50 +0700

    done

--- src/Pages/NoutFoundPages/NoutFoundPages.jsx
+++ src/Pages/NoutFoundPages/NoutFoundPages.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import "./stylepagenoutfound.css";
-
 
 const NotFoundPage = () => {
   return (
     <div id='inner' className='container'>
+      <Helmet>
+        <title>404 - Страница не найдена</title>
+        <meta name="description" content="Извините, но запрашиваемая вами страница не существует." />
+        <meta name="keywords" content="404, страница не найдена, ошибка, несуществующая страница" />
+      </Helmet>
       <h1 className='title'>404 - Страница не найдена</h1>
       <p className='message'>Извините, но запрашиваемая вами страница не существует.</p>
       <Link to="/" className="button">Вернуться на главную</Link>
